8ec190726da204fd7cb106e0982ab142fc59da90
Joel Júnior <joel.fmjr@gmail.com>
2024-12-10 14:58:10 -0300

    feat: Solution without sub vector

--- hash-2/main.cpp
+++ hash-2/main.cpp
@@ -9,7 +9,6 @@ const int TAMANHO = 1000000;
 
 int numSequencia(int n, unordered_map<int, int> &sequencias)
 {
-    vector<int> sub_sequencias;
     int sequencia = 0;
     unsigned aux = n;
 
@@ -29,32 +28,16 @@ int numSequencia(int n, unordered_map<int, int> &sequencias)
         {
             sequencia++;
             aux = aux / 2;
-            sub_sequencias.push_back(aux);
         }
         else
         {
             sequencia += 2;
-            aux = (3 * aux + 1);
-            sub_sequencias.push_back(aux);
-            aux = aux / 2;
-            sub_sequencias.push_back(aux);
+            aux = (3 * aux + 1) / 2;
         }
 
     } while (aux > 1);
 
     sequencias[n] = sequencia;
-
-    for (size_t i = 0; i < sub_sequencias.size(); i++)
-    {
-        if (sequencias.find(sub_sequencias[i]) == sequencias.end())
-        {
-            sequencias[sub_sequencias[i]] = sequencia - (i + 1);
-        }
-        else
-        {
-            break;
-        }
-    }
     return sequencia;
 }
 
@@ -67,11 +50,6 @@ int main()
 
     for (size_t i = 1; i < TAMANHO; i++)
     {
-        if (sequencias.find(i) != sequencias.end())
-        {
-            continue;
-        }
-
         int sequencia = numSequencia(i, sequencias);
 
         if (sequencia > maiorSequencia)
@@ -81,7 +59,7 @@ int main()
         }
     }
 
-    cout << maiorN << " - " << maiorSequencia << endl;
+    cout << "O número com maior sequência é " << maiorN << " com uma sequência de " << maiorSequencia << " números." << endl;
     auto end = chrono::steady_clock::now();
     auto diff = end - start;
     cout << chrono::duration<double, milli>(diff).count() << " ms" << endl;
